57c9ec57a4da0ca5de5355515b84d9b3b7637ea2
singhpriya03 <priyakushawaha73@gmail.com>
2023-03-11 19:00:31 +0545

    first commit

---
+++ hello world.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() 
+{
+   // printf() displays the string inside quotation
+   printf("Hello World!");
+   //printf("\n hi,its me Munna");
+   getch();
+}
+
